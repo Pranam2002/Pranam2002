6627b7471ddc7a50f526940a368aa3e8f36fe44a
Pranam Rohatgi <91668954+Pranam2002@users.noreply.github.com>
2025-06-12 02:46:34 +0530

    Update fetchGraph.js

--- snake-generator/fetchGraph.js
+++ snake-generator/fetchGraph.js
@@ -2,18 +2,24 @@ const fetch = require('node-fetch');
 const { JSDOM } = require('jsdom');
 
 async function fetchContributionGrid(username) {
-  const url = `https://github.com/users/${username}/contributions`;
-  const res = await fetch(url);
+  const res = await fetch(`https://github.com/users/${username}/contributions`);
   const html = await res.text();
   const dom = new JSDOM(html);
-  const rects = dom.window.document.querySelectorAll('rect');
+  const weeks = [...dom.window.document.querySelectorAll('g > g')];
 
-  const grid = Array.from(rects).map(rect => ({
-    x: parseInt(rect.getAttribute('x')),
-    y: parseInt(rect.getAttribute('y')),
-    level: parseInt(rect.getAttribute('data-level')),
-    date: rect.getAttribute('data-date')
-  }));
+  const grid = [];
+
+  weeks.forEach((week, x) => {
+    const days = week.querySelectorAll('rect');
+    days.forEach((day, y) => {
+      grid.push({
+        x,
+        y,
+        date: day.getAttribute('data-date'),
+        level: parseInt(day.getAttribute('data-level') || '0')
+      });
+    });
+  });
 
   return grid;
 }
