02a907d9a8208f11afc9aecbae4720d1dde46838
Pranam Rohatgi <91668954+Pranam2002@users.noreply.github.com>
2025-06-12 02:47:13 +0530

    Update index.js

--- snake-generator/index.js
+++ snake-generator/index.js
@@ -1,9 +1,14 @@
 const fetchGrid = require('./fetchGraph');
 const generateSVG = require('./generateSVG');
 const path = require('path');
+const fs = require('fs');
 
 (async () => {
   const username = 'Pranam2002';
   const grid = await fetchGrid(username);
-  generateSVG(grid, path.join(__dirname, '../dist/github-snake.svg'));
+
+  const dist = path.join(__dirname, '../dist');
+  fs.mkdirSync(dist, { recursive: true });
+
+  generateSVG(grid, path.join(dist, 'github-snake.svg'));
 })();
